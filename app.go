74235c6c01af7cd37063916611063a25ff702f61
amirnilofari <amirnilofari@gmail.com>
2022-04-09 00:26:42 +0430

    first commit - start with gin framework

---
+++ app.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	app := gin.Default()
+	app.SetTrustedProxies([]string{"192.168.1.2"})
+	app.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "start with gin",
+		})
+
+	})
+	app.Run()
+}
