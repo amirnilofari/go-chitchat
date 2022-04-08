1934387ef887664b51b06723a2668c9b4234fb19
amirnilofari <amirnilofari@gmail.com>
2022-04-09 00:30:37 +0430

    .

--- app.go
+++ app.go
@@ -7,7 +7,7 @@ func main() {
 	app.SetTrustedProxies([]string{"192.168.1.2"})
 	app.GET("/", func(context *gin.Context) {
 		context.JSON(200, gin.H{
-			"message": "start with gin",
+			"message": "start with gin web framework",
 		})
 
 	})
