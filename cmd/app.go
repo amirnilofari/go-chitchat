60bbe79ff78f8daf9fad0a51f4c463fee8414849
amirnilofari <amirnilofari@gmail.com>
2022-04-10 23:56:01 +0430

    add mongo

--- cmd/app.go
+++ cmd/app.go
@@ -1,15 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/amirnilofari/go-chitchat/controllers"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var mongoURI = "mongodb://localhost:27017"
 
 func main() {
 	app := gin.Default()
 	app.SetTrustedProxies([]string{"192.168.1.2"})
-	app.GET("/", func(context *gin.Context) {
-		context.JSON(200, gin.H{
-			"message": "start with Gin",
-		})
 
-	})
+	//Connect DB
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
+
+	app.GET("v1/api/posts", controllers.AllPost)
+	app.GET("v1/api/posts", controllers.CreatePost)
+	app.GET("v1/api/posts", controllers.DeletePost)
+
 	app.Run()
 }
